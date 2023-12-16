90379a42c7d6682b172d5efc38cc5524d880fc75
Dan Gurney <dan@dangurney.net>
2023-12-16 15:37:20 +0000

    feat: add wrangler.toml

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,14 @@
 /// <reference types="astro/client" />
 
-type Ai = import("@cloudflare/ai").Ai;
+type KVNamespace = import("@cloudflare/ai").Ai;
 type ENV = {
   AI: Ai;
 };
 
+// Depending on your adapter mode
+// use `AdvancedRuntime<ENV>` for advance runtime mode
+// use `DirectoryRuntime<ENV>` for directory runtime mode
 type Runtime = import("@astrojs/cloudflare").AdvancedRuntime<ENV>;
+declare namespace App {
+  interface Locals extends Runtime {}
+}
